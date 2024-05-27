33528c2547651cad14355a0f58ef3ee5040a6e9e
bayerf42 <git@bayerf.de>
2024-05-28 00:00:53 +0200

    Adapt list reverse assembly sample to new tagging scheme.

--- asm/lox_reverse.asm
+++ asm/lox_reverse.asm
@@ -8,17 +8,22 @@
 
             org     $500
 
-reverse     move.l  4(sp),d0         ; First parameter
-            andi.l  #$80000001,d0    ; Test for Object
-            bne.s   .wrong_type
+reverse     move.l  4(sp),d1         ; First parameter
+            clr.l   d0               ; dual use as bit index and return value in case of type error 
+            btst    d0,d1
+            bne.s   .wrong_type      ; it's an integer
+            cmp.l   #6,d1
+            bls.s   .wrong_type      ; it's a special value (nil, false, true)
+
             movea.l 4(sp),a1         ; Load first parameter (address of Lox list object)
             cmpi.b  #6,4(a1)         ; test for list subtype
             bne.s   .wrong_type
+
             move.w  6(a1),d0         ; length of list
             asl.w   #2,d0            ; * 4 ( = sizeof(Value) )
             movea.l 10(a1),a0        ; first item
             lea     (a0,d0.w),a1     ; beyond last item
-            asr.w   #3,d0            ; half len
+            asr.w   #3,d0            ; half length as loop counter, middle item stays fixed (odd length)
             bra.s   .loop
 
 .again      move.l  -(a1),d1         ; swap lo and hi items
@@ -27,7 +32,4 @@ reverse     move.l  4(sp),d0         ; First parameter
 .loop       dbf     d0,.again
 
             move.l  4(sp),d0         ; object itself for success
-            rts
-
-.wrong_type move.l  #$80000002,d0    ; nil for error
-            rts
+.wrong_type rts
