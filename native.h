fa7da60c3f5beaf29c2b0371fd09c463a0782edd
bayerf42 <git@bayerf.de>
2024-09-24 19:11:20 +0200

    Moved struct Native.

--- native.h
+++ native.h
@@ -1,7 +1,16 @@
 #ifndef clox_native_h
 #define clox_native_h
 
-#include "object.h"
+#include "value.h"
+
+typedef bool (*NativeFn)(int argCount, Value* args);
+
+typedef struct {
+    const char* name;
+    const char* signature;
+    NativeFn    function;
+} Native;
+
 
 void  defineAllNatives(void);
 bool  callNative(const Native* native, int argCount, Value* args);
